aea4db6ac0e750962b06929e7e99d6ca946fc88c
Ashish Kulkarni <kulkarniashish88@gmail.com>
2016-09-17 21:45:25 +0530

    Prototypes of basic functions required, added to code

---
+++ file.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * GetNextSentence (FILE *fp) {
+
+}
+
+char * GetNextWord (char *sentence, int * start) {
+
+}
---
+++ hash.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Hash (char * str) {
+
+}
+
+int InsertInHash (char *str, int index, void *table) {
+
+}
+
+int IterateTable (void * table) {
+
+}
